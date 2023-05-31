e33fc15b50326e9c5be2d03aad6d36ae9dd78f9b
longnt <nguyenlongnt71@gmail.com>
2023-05-31 16:08:52 +0700

    first commit

---
+++ dequy.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+// function loop(start, end, cb) {
+//     if (start <= end) {
+//         cb(start);
+//         loop(start + 1, end, cb);
+
+//     }
+// }
+
+
+// var course = ["JS", "Net", "PHP"];
+
+// loop(0, course.length - 1, function (index) {
+//     console.log(course[index]);
+// })
+
+
+function factical(num) {
+    var total = 1;
+    if (num != 1) {
+        total = num * factical(num - 1);
+
+    }
+    return total
+}
+
+console.log(factical(3));
